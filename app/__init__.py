303ade8948b4509b50f3aa7e6ace39cf6db024c8
Igor Eliseev <igor.eliseev@simbirsoft.com>
2022-09-12 23:41:38 +0400

    gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from config import Config
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 config = Config()
 app.config.from_object(config)
+Bootstrap(app)
 
 from . import routes
