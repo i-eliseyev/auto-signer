ab53277407d186f751137288d446936044a7235c
Igor Eliseev <igor.eliseev@simbirsoft.com>
2022-09-17 16:55:39 +0400

    update

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 from PIL import Image
 from PIL.PngImagePlugin import PngImageFile
+import pytesseract
 
 
-def sign(image: PngImageFile):
-    sign_fixed_width = 150
-    prin_fixed_width = 200
+def sign(image: PngImageFile, original_filename: str):
+    target = 'Бобылев'
+    data = pytesseract.image_to_data(
+        image,
+        output_type=pytesseract.Output.DICT,
+        lang='rus',
+    )
+    word_occurences = [i for i, word in enumerate(data["text"]) if word == target]
+    if len(word_occurences) != 1:
+        occ = word_occurences[-1]
+    else:
+        occ = word_occurences[0]
+    l = data["left"][occ]
+    t = data["top"][occ]
+
+    print_x = l + 380
+    sign_x = l + 30
+    print_y = t - 250
+    sign_y = t - 200
+
+    print_fixed_width = 280
     sign = Image.open('sign.png')
-    width_percent_sign = (sign_fixed_width / float(sign.size[0]))
-    height_size_sign = int((float(sign.size[0]) * float(width_percent_sign)))
     _print = Image.open('print.png')
-    width_percent_print = (prin_fixed_width / float(_print.size[0]))
+    width_percent_print = (print_fixed_width / float(_print.size[0]))
     height_size_print = int((float(_print.size[0]) * float(width_percent_print)))
 
-    sign = sign.resize((sign_fixed_width, height_size_sign))
-    _print = _print.resize((prin_fixed_width, height_size_print))
-    image.paste(sign, (250, 1190),  sign)
-    image.paste(_print, (400, 1190), _print)
-    image.filename = image.filename[:-3] + 'pdf'
+    sign = sign.resize((320, 150))
+    _print = _print.resize((print_fixed_width, height_size_print))
+    image.paste(sign, (sign_x, sign_y),  sign)
+    image.paste(_print, (print_x, print_y), _print)
+    image.filename = original_filename
     return image
